afe8e0afeb069df547558b1fb1fee6a96f221726
Tomasz Debowski <tomaszdebowski88@gmail.com>
2024-12-14 01:30:05 +0100

    add acces token

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+AMADEUS_API_KEY=os.environ.get('AMADEUS_API_KEY')
+AMADEUS_API_SECRET=os.environ.get('AMADEUS_API_SECRET')
+AMADEUS_TOKEN_URL='https://test.api.amadeus.com/v1/security/oauth2/token'
+
+token_body={
+    'client_id':AMADEUS_API_KEY,
+    'client_secret':AMADEUS_API_SECRET,
+    'grant_type':'client_credentials'
+}
+
+response=requests.post(url=AMADEUS_TOKEN_URL,data=token_body)
+access_token=response.json()['access_token']
+
+
