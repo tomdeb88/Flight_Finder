478b60e80e07a97b808d901763c257aa560bf97e
Tomasz Debowski <tomaszdebowski88@gmail.com>
2024-12-14 15:14:13 +0100

    get sheet data

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,22 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+from pprint import pprint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SHEETY_ENDPOINT="https://api.sheety.co/96fd27c8901e7407d06fb5078cdc3e3d/flightDeals/prices"
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.username=os.environ.get('SHEETY_USERNAME')
+        self.password=os.environ.get('SHEETY_PASSWD')
+        self.authorization=HTTPBasicAuth(username=self.username,password=self.password)
+        self.cities_data={}
+        self.get_cities_data()
+
+    def get_cities_data(self):
+        response=requests.get(url=SHEETY_ENDPOINT,auth=self.authorization)
+        data=response.json()
+        self.cities_data=data['prices']
--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-import requests
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-AMADEUS_API_KEY=os.environ.get('AMADEUS_API_KEY')
-AMADEUS_API_SECRET=os.environ.get('AMADEUS_API_SECRET')
-AMADEUS_TOKEN_URL='https://test.api.amadeus.com/v1/security/oauth2/token'
-
-token_body={
-    'client_id':AMADEUS_API_KEY,
-    'client_secret':AMADEUS_API_SECRET,
-    'grant_type':'client_credentials'
-}
-
-response=requests.post(url=AMADEUS_TOKEN_URL,data=token_body)
-access_token=response.json()['access_token']
+from data_manager import DataManager
 
 
